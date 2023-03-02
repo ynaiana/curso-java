2bad8a71adeb314fb8ce879894a5721a08b3dcaa
ynaiana <ynaiana.s1@gmail.com>
2023-03-02 20:30:47 -0300

    Aula 02 - Exercícios

---
+++ Aula02_DECISAO/src/Exercicio01.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        double nota1 = 0, nota2 = 0, nota3 = 0, nota4 = 0;
+        double media = 0;
+
+        System.out.println("Informe a 1ª nota: ");
+        nota1 = leia.nextDouble();
+
+        System.out.println("Informe a 2ª nota: ");
+        nota2 = leia.nextDouble();
+
+        System.out.println("Informe a 3ª nota: ");
+        nota3 = leia.nextDouble();
+
+        System.out.println("Informe a 4ª nota: ");
+        nota4 = leia.nextDouble();
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        if (media >= 6){
+            System.out.println("Aprovado!");
+        } else if (media < 6 &&  media > 5) {
+            System.out.println("Recuperação!");
+        } else {
+            System.out.println("Reprovado!");
+        }
+    }
+}
+// EXERCÍCIO 01  - AULA 02
+// Algoritmo que receba 4 notas e verifique se a média é maior ou igual a 6. Se for maior ou igual a
+// seis apresente a mensagem de "Aprovado". Se for menor apresente "Reprovado".
+// (Aventureiro:  Se for menor que 6 e maior que 5 então apresente "Recuperação") .
---
+++ Aula02_DECISAO/src/Exercicio04.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        int opcao = 0;
+
+        System.out.println("Digite 1 para calcular a área do quadrado");
+        System.out.println("Digite 2 para calcular a área do retangulo");
+        System.out.println("Digite 3 para calcular a área do círculo");
+        opcao = leia.nextInt();
+
+        switch (opcao){
+            case 1: {
+                int lado, areaQuadrado;
+                System.out.println("Informe o lado do quadrado: ");
+                lado = leia.nextInt();
+                areaQuadrado = lado * lado;
+                System.out.println("A área do quadrado é: " +areaQuadrado); break;
+            }
+
+            case 2: {
+                int altura, comprimento, areaRetangulo;
+                System.out.println("Informe a altura do retangulo: ");
+                altura = leia.nextInt();
+                System.out.println("Informe o comprimento do retangulo: ");
+                comprimento = leia.nextInt();
+                areaRetangulo = altura * comprimento;
+                System.out.println("A área do retangulo é: " +areaRetangulo); break;
+            }
+
+            case 3: {
+                double raio, areaCirculo;
+                System.out.println("Informe o raio do circulo: ");
+                raio = leia.nextInt();
+                areaCirculo = Math.PI * Math.pow(raio, 2);
+                System.out.println("A área do circulo é: " +areaCirculo); break;
+            }
+            default: {
+                System.out.println("Opção inválida!");
+            }
+        }
+    }
+}
+//EXERCÍCIO 04 - AULA 02
+//
+//Construa um algoritmo que possa calcular a área de objetos geometricos (quadradro/retangulo/circulo).
+// Faça com que o usuario informe primeiro qual a forma ele quer calcular e depois receba os valores
+// necessários e realize o calculo.
+//ex:
+//Digite 1 para calcular a area do quadrado;
+//Digite 2 para calcular a area do retangulo;
+//Digite 3 para calcular a area do circulo;
+//Qualquer outra opção escreva "Opção inválida"
